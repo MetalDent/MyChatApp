323cb80c80e6e7123c2ae9ac67419817827e5bd4
MetalDent <priyapandya274@gmail.com>
2019-05-29 17:05:35 +0530

    Solve gradle issues

--- app/src/main/java/com/metalDent/mychatapp/ChatMessage.java
+++ app/src/main/java/com/metalDent/mychatapp/ChatMessage.java
@@ -3,28 +3,21 @@ package com.metalDent.mychatapp;
 import java.util.Date;
 
 public class ChatMessage {
+
     private String messageText;
     private String messageUser;
-    private String messageUserId;
     private long messageTime;
 
-    public ChatMessage(String messageText, String messageUser, String messageUserId) {
+    public ChatMessage(String messageText, String messageUser) {
         this.messageText = messageText;
         this.messageUser = messageUser;
+
+        // Initialize to current time
         messageTime = new Date().getTime();
-        this.messageUserId = messageUserId;
     }
 
     public ChatMessage(){
 
-    }
-
-    public String getMessageUserId() {
-        return messageUserId;
-    }
-
-    public void setMessageUserId(String messageUserId) {
-        this.messageUserId = messageUserId;
     }
 
     public String getMessageText() {
--- app/src/main/java/com/metalDent/mychatapp/MainActivity.java
+++ app/src/main/java/com/metalDent/mychatapp/MainActivity.java
@@ -5,12 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -23,62 +24,103 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
     private static final int SIGN_IN_REQUEST_CODE = 111;
     private FirebaseListAdapter<ChatMessage> adapter;
-    private ListView listView;
-    private String loggedInUserName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //find views by Ids
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        final EditText input = (EditText) findViewById(R.id.input);
-        listView = (ListView) findViewById(R.id.list);
-
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
             // Start sign in/sign up activity
-            startActivityForResult(AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .build(), SIGN_IN_REQUEST_CODE);
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    SIGN_IN_REQUEST_CODE
+            );
         } else {
-            // User is already signed in, show list of messages
-            showAllOldMessages();
+            // User is already signed in. Therefore, display
+            // a welcome Toast
+            Toast.makeText(this,
+                    "Welcome " + FirebaseAuth.getInstance()
+                            .getCurrentUser()
+                            .getDisplayName(),
+                    Toast.LENGTH_LONG)
+                    .show();
+
+            // Load chat room contents
+            displayChatMessages();
         }
+
+        FloatingActionButton fab =
+                (FloatingActionButton)findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (input.getText().toString().trim().equals("")) {
-                    Toast.makeText(MainActivity.this, "Please enter some texts!", Toast.LENGTH_SHORT).show();
-                } else {
-                    FirebaseDatabase.getInstance()
-                            .getReference()
-                            .push()
-                            .setValue(new ChatMessage(input.getText().toString(),
-                                    FirebaseAuth.getInstance().getCurrentUser().getDisplayName(),
-                                    FirebaseAuth.getInstance().getCurrentUser().getUid())
-                            );
-                    input.setText("");
-                }
+                EditText input = (EditText)findViewById(R.id.input);
+
+                // Read the input field and push a new instance
+                // of ChatMessage to the Firebase database
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                // Clear the input
+                input.setText("");
             }
         });
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN_REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        "Successfully signed in. Welcome!",
+                        Toast.LENGTH_LONG)
+                        .show();
+                displayChatMessages();
+            } else {
+                Toast.makeText(this,
+                        "We couldn't sign you in. Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show();
+
+                // Close the app
+                finish();
+            }
+        }
+
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_sign_out) {
+        if(item.getItemId() == R.id.menu_sign_out) {
             AuthUI.getInstance().signOut(this)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            Toast.makeText(MainActivity.this, "You have logged out!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this,
+                                    "You have been signed out.",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+
+                            // Close activity
                             finish();
                         }
                     });
@@ -86,33 +128,30 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == SIGN_IN_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                Toast.makeText(this, "Signed in successful!", Toast.LENGTH_LONG).show();
-                showAllOldMessages();
-            } else {
-                Toast.makeText(this, "Sign in failed, please try again later", Toast.LENGTH_LONG).show();
+    private void displayChatMessages() {
+        ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
 
-                // Close the app
-                finish();
+        adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
+                R.layout.message, FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                // Get references to the views of message.xml
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+
+                // Set their text
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                // Format the date before showing it
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
             }
-        }
+        };
+
+        listOfMessages.setAdapter(adapter);
     }
 
-    private void showAllOldMessages() {
-        loggedInUserName = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        Log.d("Main", "user id: " + loggedInUserName);
-
-        adapter = new MessageAdapter(this, ChatMessage.class, R.layout.item_in_message,
-                FirebaseDatabase.getInstance().getReference());
-        listView.setAdapter(adapter);
-    }
-
-    public String getLoggedInUserName() {
-        return loggedInUserName;
-    }
 }
